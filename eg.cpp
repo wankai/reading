c9aed8447d5b5d648828d8c52d339a31c2e1e19e
Wankai Zhang <wankaizhang@gmail.com>
2015-11-22 12:02:44 +0800

    Update eg.cpp

--- eg.cpp
+++ eg.cpp
@@ -3,13 +3,13 @@ class Singleton
  public:
   static Singleton* instance()
   {
-    Singleton* tmp = interpret_cast<Singleton*>(ptr_.load(std::memory_order_acquire));
+    Singleton* tmp = ptr_.load(std::memory_order_acquire));
     if (tmp == NULL) {
       {
         MutexGuard mg(mutex_);
         if (ptr_ == NULL) {
-          Singleton* t = new Singleton;
-          ptr_.store(t, std::memory_order_release);
+          tmp = new Singleton;
+          ptr_.store(tmp, std::memory_order_release);
         }
       }
       local_ptr_ = ptr_;
@@ -20,5 +20,5 @@ class Singleton
  private:
   Singleton();
   static Mutex mutex_;
-  static std::atomic<uintptr_t> ptr_;
+  static AtomicPtr<Singleton> ptr_;
 };
