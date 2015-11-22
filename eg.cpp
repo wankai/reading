4e133521c7c9298c5504fe0a5e4aafdddb2e8116
Wankai Zhang <wankaizhang@gmail.com>
2015-11-22 11:21:15 +0800

    Update eg.cpp

--- eg.cpp
+++ eg.cpp
@@ -3,11 +3,21 @@ class Singleton
  public:
   static Singleton* instance()
   {
-    static Singleton obj;
-    return &obj;
+    if (!local_ptr_) {
+      {
+        MutexGuard mg(mutex_);
+        if (ptr_ == NULL) {
+          ptr_ = new Singleton;
+        }
+      }
+      local_ptr_ = ptr_;
+    }
+    return local_ptr_.get();
   }
   
  private:
   Singleton();
+  thread_local SharedPtr<Singleton> local_ptr_;
+  static Mutex mutex_;
+  static Singleton* ptr_;
 };
-
